0ddf8998da9dc0693b70c161a7509df60c1509ef
bitfexl <54662051+bitfexl@users.noreply.github.com>
2024-10-05 23:39:39 +0200

    Update entrypoint.sh

--- overpass/entrypoint.sh
+++ overpass/entrypoint.sh
@@ -6,26 +6,26 @@ if ! [[ -d /data/db ]]
 then
     if [[ ${DATA_URL: -8} == ".osm.pbf" ]]
     then
-        wget $DATA_URL -O data.osm.pbf
+        wget $DATA_URL -O /data/data.osm.pbf
         
         # https://osmcode.org/osmium-tool/manual.html#osm-file-formats-and-converting-between-them
         
         echo "Converting data..."
-        osmium cat data.osm.pbf -o data.osm.bz2
+        osmium cat /data/data.osm.pbf -o /data/data.osm.bz2
 
-        rm data.osm.pbf
+        rm /data/data.osm.pbf
     elif [[ ${DATA_URL: -8} == ".osm.bz2" ]]
     then
-        wget $DATA_URL -O data.osm.bz2
+        wget $DATA_URL -O /data/data.osm.bz2
     else
         echo "Error DATA_URL: only *.osm.pbf and *.osm.bz2 data sources are allowed."
         exit 1
     fi
 
     echo "Loading data..."
-    bin/init_osm3s.sh data.osm.bz2 /data/db .
+    bin/init_osm3s.sh /data/data.osm.bz2 /data/db .
 
-    rm data.osm.bz2
+    rm /data/data.osm.bz2
 fi
 
 # Start overpass
